d6959538335c43b1bb980f62d8f060accb6ffd58
aman1722 <112754413+aman1722@users.noreply.github.com>
2023-09-22 10:01:34 +0530

    User Registaration Logic Complete

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,3 +1,6 @@
+const { UserModel } = require("../models/user.model");
+const bcrypt = require("bcrypt");
+
 
 
 
@@ -5,7 +8,22 @@
 
 
 const register = async (req,res)=>{
-        
+        try {
+            const {email,password} = req.body;
+            const isUserAlreadyExists = await UserModel.findOne({email});
+
+            if(isUserAlreadyExists) return res.status(400).send({msg:"User Already Exists"});
+
+            const hashPswword = await bcrypt.hash(password,Number(process.env.SALT_ROUNDS));
+
+            const newUser = await new UserModel({...req.body,password:hashPswword});
+            await newUser.save();
+
+            res.status(201).send({msg:"User Registration Successful"})
+        } catch (error) {
+            console.log(error.message);
+            res.status(501).send({msg:error.message});
+        }
 }
 
 
