10eb212f1b20bcb680f1e6fc088c735823fad35f
aman1722 <112754413+aman1722@users.noreply.github.com>
2023-09-22 15:37:44 +0530

    User routes vlidation Logic Complete

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -2,76 +2,80 @@ const { UserModel } = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { blacklistModel } = require("../models/blacklist.model");
+const { validationResult } = require("express-validator");
 
 
 
 
 
 
+const register = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).send({ errors: errors.array() });
+    }
+    try {
+        const { email, password } = req.body;
+        const isUserAlreadyExists = await UserModel.findOne({ email });
 
-const register = async (req,res)=>{
-        try {
-            const {email,password} = req.body;
-            const isUserAlreadyExists = await UserModel.findOne({email});
+        if (isUserAlreadyExists) return res.status(400).send({ msg: "User Already Exists" });
 
-            if(isUserAlreadyExists) return res.status(400).send({msg:"User Already Exists"});
+        const hashPswword = await bcrypt.hash(password, Number(process.env.SALT_ROUNDS));
 
-            const hashPswword = await bcrypt.hash(password,Number(process.env.SALT_ROUNDS));
+        const newUser = await new UserModel({ ...req.body, password: hashPswword });
+        await newUser.save();
 
-            const newUser = await new UserModel({...req.body,password:hashPswword});
-            await newUser.save();
-
-            res.status(201).send({msg:"User Registration Successful"})
-        } catch (error) {
-            console.log('/user/register: ', err.message);
-            res.status(501).send({msg:"Internal Server error",error:error.message});
-        }
-}
-
-
-const login = async (req,res)=>{
-        try {
-            const {email,password} = req.body;
-            const userExists = await UserModel.findOne({email});;
-            
-            if(!userExists) return res.status(400).send({msg:"User not exixts! Plaese register first"});
-
-            const isPasswordCorrect = await bcrypt.compare(password, userExists.password);
-
-            if(!isPasswordCorrect) return res.status(400).send({msg:"Incorrect Password!"});;
-
-            const token = jwt.sign(
-                { user_id: userExists._id, role: userExists.role },
-                process.env.JWT_LOGIN_SECRET,
-                {
-                  expiresIn: "3h",
-                }
-            );
-          
-            res.status(200).json({ message: "Login Successful", token });
-        } catch (error) {
-            console.log('/user/login: ', err.message);
-            res.status(501).send({msg:"Internal Server error",error:error.message});
-        }
-}
-
-
-
-const logout = async (req,res)=>{
-    try{
-        const token = req.headers.authorization.split(' ')[1] || req.headers.authorization;
-        const blacklisted = new blacklistModel({"token": token});
-        await blacklisted.save();
-        console.log('logout successful')
-        res.status(200).send({msg: 'Logout Successful'});
-    }catch(error){
-        console.log('/user/logout: ', error.message);
-        res.status(501).send({msg:"Internal Server error",error:error.message});
+        res.status(201).send({ msg: "User Registration Successful" })
+    } catch (error) {
+        console.log('/user/register: ', err.message);
+        res.status(501).send({ msg: "Internal Server error", error: error.message });
     }
 }
 
 
-module.exports={
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const userExists = await UserModel.findOne({ email });;
+
+        if (!userExists) return res.status(400).send({ msg: "User not exixts! Plaese register first" });
+
+        const isPasswordCorrect = await bcrypt.compare(password, userExists.password);
+
+        if (!isPasswordCorrect) return res.status(400).send({ msg: "Incorrect Password!" });;
+
+        const token = jwt.sign(
+            { user_id: userExists._id, role: userExists.role },
+            process.env.JWT_LOGIN_SECRET,
+            {
+                expiresIn: "3h",
+            }
+        );
+
+        res.status(200).json({ message: "Login Successful", token });
+    } catch (error) {
+        console.log('/user/login: ', err.message);
+        res.status(501).send({ msg: "Internal Server error", error: error.message });
+    }
+}
+
+
+
+const logout = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1] || req.headers.authorization;
+        const blacklisted = new blacklistModel({ "token": token });
+        await blacklisted.save();
+        console.log('logout successful')
+        res.status(200).send({ msg: 'Logout Successful' });
+    } catch (error) {
+        console.log('/user/logout: ', error.message);
+        res.status(501).send({ msg: "Internal Server error", error: error.message });
+    }
+}
+
+
+module.exports = {
     register,
     login,
     logout
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { query } = require("express-validator");
+const { body } = require("express-validator");
 const { register, login, logout } = require("../controllers/user.controllers");
 
 
@@ -8,16 +8,43 @@ const userRouter = express.Router();
 
 
 
-userRouter.post("/register",register);
+userRouter.post("/register", [
+    body("username")
+        .notEmpty()
+        .withMessage("Username is required")
+        .isLength({ min: 3 })
+        .withMessage("Username must be atleast 3 characters"),
+    body("email")
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("Please provide a valid email"),
+    body("password")
+        .notEmpty()
+        .withMessage("Password is required")
+        .isLength({ min: 6 })
+        .withMessage("Password must be at least 6 characters"),
+], register);
 
-userRouter.post("/login",login);
+userRouter.post("/login",[
+    body("email")
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Please provide a valid email"),
+    body("password")
+      .notEmpty()
+      .withMessage("Password is required")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters"),
+  ], login);
 
-userRouter.post("logout",logout);
+userRouter.post("/logout", logout);
 
 
 
 
 
-module.exports={
+module.exports = {
     userRouter
 }
